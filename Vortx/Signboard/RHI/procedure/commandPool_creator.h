a1a6233d43d245a1d0534683462adbecd2ae9c1e
ajaypartap_jattana <ajaypartapjattana@gmail,com>
2026-02-05 19:37:09 +0530

    m_

--- Vortx/Signboard/RHI/procedure/commandPool_creator.h
+++ Vortx/Signboard/RHI/procedure/commandPool_creator.h
@@ -13,24 +13,16 @@ namespace rhi::procedure {
 
 	class commandPool_creator {
 	public:
+		commandPool_creator(const rhi::core::device& device);
+
 		struct pool_requirement {
 			uint32_t family;
 			VkQueueFlags capabilities;
 			bool present_supported;
 		};
 
-		commandPool_creator(const rhi::core::device& device);
-
-		uint32_t get_requiredPoolCount() noexcept;
-		//const std::vector<pool_requirement>& get_poolRequirements() noexcept;
-
-		/*struct pool_requirement {
-			uint32_t family;
-			VkQueueFlags capabilities;
-			bool present_supported;
-		};*/
-
 		const std::vector<pool_requirement>& get_poolRequirements() noexcept;
+		uint32_t get_requiredPoolCount() noexcept;
 
 		VkResult create(rhi::core::commandPool* pools, uint32_t poolCount);
 
